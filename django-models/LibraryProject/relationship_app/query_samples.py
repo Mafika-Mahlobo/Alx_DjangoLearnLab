f826b9b77f215cad150c7f1a951e7609ab6eda33
Mafika <rudolphmafika@gmail.com>
2025-09-07 14:39:44 +0200

    Fetch books query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,9 @@ from .models import Author, Book, Library, Librarian
 john = Author.objects.get(name="John Green")
 Book.objects.filter(author=john).values()
 
-Book.objects.all().values()
+library_name = "New Library"
+books = Library.objects.get(name=library_name)
+books.objects.all()
 
-new_library = Library.objects.get(name="New Library")
+new_library = Library.objects.get(name=library_name)
 Librarian.objects.filter(library=new_library).values()
