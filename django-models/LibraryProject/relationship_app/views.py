e9badf7af59237bbe9ce56b598000a1671536090
Mafika Mahlobo <rudolphmafika@gmail.com>
2025-09-09 22:47:13 +0200

    Task2. User authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from  .models import Book, Author
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.models import User 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 
