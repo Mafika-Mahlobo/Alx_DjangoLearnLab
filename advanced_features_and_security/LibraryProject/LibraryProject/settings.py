75581f7351cff783681a30d1f1544064e2ca0115
Mafika Mahlobo <rudolphmafika@gmail.com>
2025-09-13 14:43:16 +0200

    Taks0

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 
 class BoookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author', 'publication_year')
     list_filter = ('title', 'author', 'publication_year')
 
+
+class CustomUserManager(admin.ModelAdmin):
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    fieldsets = (
+
+            (None, {"fields": ("username", "email", "password", "date_of_birth", "profile_phone")}),
+            ("permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        )
+
+
 admin.site.register(Book, BoookAdmin)
+admin.site.register(CustomUser, CustomUserManager)
