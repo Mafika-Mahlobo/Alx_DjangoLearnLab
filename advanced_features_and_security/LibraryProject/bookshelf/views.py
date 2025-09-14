c2df38cabddcad735aaa7e52c5f15e225d9f2589
Mafika Mahlobo <rudolphmafika@gmail.com>
2025-09-14 12:22:14 +0200

    User permission_required in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.decorators import permission_required
 
 
 
+@permission_required("bookshelf.can_view", raise_exception=True)
 def book_list_view(request):
-	if request.user.has_perm("bookshelf.can_view"):
-		return HttpResponse("You are allowed ro view books!")
-	return HttpResponse("Access denied!")
+	return HttpResponse("You are allowed ro view books!")
+	
 
-
-
+@permission_required("bookshelf.can_create", raise_exception=True)
 def book_add_view(request):
-	if request.user.has_perm("bookshelf.can_create"):
-		return HttpResponse("You can go ahead and add a book")
-	return HttpResponse("Access denied!")
+	return HttpResponse("You can go ahead and add a book")
+	
 
-
+@permission_required("bookshelf.can_edit", raise_exception=True)
 def book_edit_view(request):
-	if request.user.has_perm("bookshelf.can_edit"):
-		return HttpResponse("You can go ahead and edit books")
-	return HttpResponse("Access denied!")
+	return HttpResponse("You can go ahead and edit books")
 
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
 def book_delete_view(request):
-	if request.user.has_perm("bookshelf.can_delete"):
-		return HttpResponse("You can delete a book")
-	return HttpResponse("Access denied!")
+	return HttpResponse("You can delete a book")
+	
